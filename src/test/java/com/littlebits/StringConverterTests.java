ee06aa82ae71edc73c40f30f339e064c9b4962d4
jjaniak <julia.janiak@gmail.com>
2020-06-08 01:22:09 +0200

    minor corrections improving tests

--- src/test/java/com/littlebits/StringConverterTests.java
+++ src/test/java/com/littlebits/StringConverterTests.java
@@ -11,16 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringConverterTests {
-    private StringConverter converter = new StringConverter();
-    private List<String> list = new ArrayList<>();
+    private final StringConverter converter = new StringConverter();
     private static final String EMPTY = "";
-
-// Write a method that will convert input list of strings into single String.
-// BeforeEach ?  - make sure the list is empty before each test
 
     @Test
     public void shouldConvertEmptyListToEmptyString() {
-        String actualResult = converter.convertListToSingleString(list);
+        String actualResult = converter.convertListToSingleString(new ArrayList<>());
 
         assertEquals(EMPTY, actualResult);
     }
@@ -28,29 +24,23 @@ public class StringConverterTests {
     @Test
     public void shouldConvertListOfEmptyElementsToEmptyString() {
         String[] elements = {"", "", "", "", ""};
-        list.addAll(Arrays.asList(elements) );
-        String actualResult = converter.convertListToSingleString(list);
+        String actualResult = converter.convertListToSingleString(Arrays.asList(elements));
 
         assertEquals(EMPTY, actualResult);
     }
 
     @Test
     public void shouldConvertListOfStringsToSingleString() {
-        String expectedResult = "Maryhadalittlelamb";
-        String[] elements = {"Mary", "had", "a", "little", "lamb", "."};
-        list.addAll(Arrays.asList(elements) );
-
-        // or
-        // list = List.of("Mary", "had", "a", "little", "lamb", ".");
-
-        String actualResult = converter.convertListToSingleString(list);
+        String[] elements = {"Mary ", "had", " a ", "little", "lamb", ".  "};
+        final String expectedResult = "Mary had a littlelamb.  ";
+        String actualResult = converter.convertListToSingleString(Arrays.asList(elements));
 
         assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void shouldConvertListOfSpecialCharStringsToSingleString() {
-        list = List.of("ਬੀਟਲ", "على", "źdble", "сярод", "pcheł", "!");
+        List<String> list = List.of("ਬੀਟਲ", "على", "źdble", "сярод", "pcheł", "!"); // todo: List.of or Arrays.asList
         final String expectedResult = "ਬੀਟਲعلىźdbleсяродpcheł!";
         String actualResult = converter.convertListToSingleString(list);
 
