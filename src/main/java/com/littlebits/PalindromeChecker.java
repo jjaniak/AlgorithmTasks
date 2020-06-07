8f426c7cc1e743dfc123d8ce0d8d1e11c256da15
jjaniak <julia.janiak@gmail.com>
2020-06-08 01:54:27 +0200

    create PalindromeChecker

---
+++ src/main/java/com/littlebits/PalindromeChecker.java
@@ -0,0 +1,24 @@
+package com.littlebits;
+
+public class PalindromeChecker {
+    public boolean isPalindrome(String input) {
+        if (input == null) {
+            return false;
+        }
+        // punctuation and spaces are ignored when checking for palindromes
+        input = input.replaceAll("[\\p{Punct}\\s]","").toLowerCase();
+
+        // empty String (or String with only spaces or punctuation) is not considered a palindrome
+        if (input.isEmpty()) {
+            return false;
+        }
+
+        int length = input.length();
+        for (int i = 0; i < length /2; i++) {
+            if (input.charAt(i) != input.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
