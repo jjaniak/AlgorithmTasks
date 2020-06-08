ec81af225b2e82fbe56cfa767bb0872d68c3e3a7
jjaniak <julia.janiak@gmail.com>
2020-06-08 17:41:25 +0200

    create SumChecker

---
+++ src/main/java/com/littlebits/SumChecker.java
@@ -0,0 +1,19 @@
+package com.littlebits;
+
+public class SumChecker {
+
+    public static boolean isThereSumWhenAddingTwoNumbers(int x, int[] numbers) {
+        if (numbers == null || numbers.length < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == x) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
